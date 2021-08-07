75a4cab0ebdb45998e42dc43ae86a862e0336f67
thongn <thongn@logigear.com>
2021-08-06 23:00:07 -0700

    show the search component and progress bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SearchData} from "./movie-search/movie-search.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'movies-ng';
+  title = 'Movies Go'
+  searching = false
+
+  onSearch(data: SearchData) {
+
+  }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,6 +4,7 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
 
 @NgModule({
   exports: [
@@ -12,6 +13,7 @@ import {MatSelectModule} from "@angular/material/select";
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatProgressBarModule,
   ]
 })
 export class AppMaterialModule { }
